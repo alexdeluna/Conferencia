975df30a44c8f54c2dec1b86723be8aa25c13d39
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-09-24 15:13:37 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCtKvUZOhXhPdRMXdEQqzJX7uoddn6fl48",
-  authDomain: "conferencia-patrimonial.firebaseapp.com",
-  projectId: "conferencia-patrimonial",
-  storageBucket: "conferencia-patrimonial.firebasestorage.app",
-  messagingSenderId: "229494189236",
-  appId: "1:229494189236web:9f977bc49a272097913be1"
-};
